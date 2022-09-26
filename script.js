a7c2016a9a41888c2f083da35d8f49f53c51b798
Aljoscha Frey <aljoscha.fry@gmail.com>
2022-09-26 21:14:22 +0200

    Create a simple moving rectangle

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+let position;
+let velocity;
+
 function setup() {
-    createCanvas(displayWidth, displayHeight);
+    createCanvas(windowWidth, windowHeight);
+    frameRate(30);
+
+    position = createVector(width / 2, height / 2);
+
+    velocity = createVector(.3, 1);
 }
 
 function draw() {
     background(220);
+
+    fill(color(200, 0, 200));
+    rect(position.x, position.y, 50, 50);
+    position.add(velocity);
+
 }
